89a35389f66554815eada5470f45f6c8f11809a5
Mikulash <62067494+mikulash@users.noreply.github.com>
2024-10-30 17:26:52 +0100

    fix: notification checking

--- TranslationManagement.Api/Controllers/TranslationJobController.cs
+++ TranslationManagement.Api/Controllers/TranslationJobController.cs
@@ -8,88 +8,95 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace TranslationManagement.Api.Controllers
+namespace TranslationManagement.Api.Controllers;
+
+[ApiController]
+[Route("api/jobs/[action]")]
+public class TranslationJobController(
+    ITranslationJobService translationJobService,
+    ILogger<TranslatorManagementController> logger)
+    : ControllerBase
 {
-    [ApiController]
-    [Route("api/jobs/[action]")]
-    public class TranslationJobController(
-        ITranslationJobService translationJobService,
-        ILogger<TranslatorManagementController> logger)
-        : ControllerBase
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<TranslationJobDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetJobs()
     {
-        [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<TranslationJobDto>), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult GetJobs()
+        var jobs = translationJobService.GetJobs();
+        if (jobs.Length == 0) return NotFound();
+
+        return Ok(jobs);
+    }
+
+
+    [HttpPost]
+    [ProducesResponseType(typeof(IEnumerable<TranslationJobDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult CreateJob(CreateTranslationJobDto job)
+    {
+        var retval = translationJobService.CreateTranslationJob(job);
+        if (retval)
         {
-            var jobs = translationJobService.GetJobs();
-            if (jobs.Length == 0)
+            var notificationSvc = new UnreliableNotificationService();
+            const int maxRetries = 3;
+            var attempts = 0;
+            while (attempts < maxRetries)
             {
-                return NotFound();
-            }
-
-            return Ok(jobs);
-        }
-
-
-        [HttpPost]
-        [ProducesResponseType(typeof(IEnumerable<TranslationJobDto>), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult CreateJob(CreateTranslationJobDto job)
-        {
-            var retval = translationJobService.CreateTranslationJob(job);
-            if (retval)
-            {
-                var notificationSvc = new UnreliableNotificationService();
-                while (!notificationSvc.SendNotification("Job created: " + job.Id).Result)
+                try
                 {
-                    // todo fix this retry logic
+                    var result = notificationSvc.SendNotification("Job created: " + job.Id).Result;
+                    if (result) return Ok();
+                }
+                catch (ApplicationException ex)
+                {
+                    logger.LogError(ex, "Attempt {Attempt} failed with error: {ErrorMessage}", attempts + 1,
+                        ex.Message);
                 }
 
-                logger.LogInformation("New job notification sent");
-                return Ok();
+                attempts++;
             }
 
-            return BadRequest();
-
+            return Ok();
         }
 
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult CreateJobWithFile(IFormFile file, string customer)
-        {
-            try
-            {
-                var result = translationJobService.CreateJobWithFile(file, customer);
-                return result ? Ok() : BadRequest();
-            }
-            catch (InvalidOperationException e)
-            {
-                logger.LogError(e, e.Message);
-                return BadRequest(e.Message);
-            }
-            catch (NotSupportedException e)
-            {
-                logger.LogError(e, e.Message);
-                return BadRequest(e.Message);
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, e.Message);
-                return BadRequest(e.Message);
-            }
-        }
+        return BadRequest();
+    }
 
-        [HttpPut]
-        public IActionResult UpdateJobStatus(int jobId, int translatorId, JobStatus newStatus = JobStatus.New)
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult CreateJobWithFile(IFormFile file, string customer)
+    {
+        try
         {
-            logger.LogInformation(
-                "Job status update request received: {NewStatus} for job {JobId} by translator {TranslatorId}",
-                newStatus, jobId, translatorId);
-            var result = translationJobService.UpdateJobStatus(jobId, newStatus);
+            var result = translationJobService.CreateJobWithFile(file, customer);
             return result ? Ok() : BadRequest();
-
         }
+        catch (InvalidOperationException e)
+        {
+            logger.LogError(e, e.Message);
+            return BadRequest(e.Message);
+        }
+        catch (NotSupportedException e)
+        {
+            logger.LogError(e, e.Message);
+            return BadRequest(e.Message);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, e.Message);
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPut]
+    public IActionResult UpdateJobStatus(int jobId, int translatorId, JobStatus newStatus = JobStatus.New)
+    {
+        logger.LogInformation(
+            "Job status update request received: {NewStatus} for job {JobId} by translator {TranslatorId}",
+            newStatus, jobId, translatorId);
+        var result = translationJobService.UpdateJobStatus(jobId, newStatus);
+        return result ? Ok() : BadRequest();
+
     }
 }
