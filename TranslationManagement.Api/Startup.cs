bc1a65e06625c41542f28c0740a827da8aa7051d
Mikulash <62067494+mikulash@users.noreply.github.com>
2024-10-31 14:56:36 +0100

    feat: easier enum use on endpoints

--- TranslationManagement.Api/Startup.cs
+++ TranslationManagement.Api/Startup.cs
@@ -1,51 +1,52 @@
+using System.Text.Json.Serialization;
 using BusinessLayer.Services;
 using DataAccessLayer;
 using DataAccessLayer.UnitOfWork;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
 
-namespace TranslationManagement.Api
+namespace TranslationManagement.Api;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers().AddJsonOptions(options =>
         {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
+            // This will serialize enums as strings
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+        services.AddSwaggerGen(c =>
         {
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TranslationManagement.Api", Version = "v1" });
-            });
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "TranslationManagement.Api", Version = "v1" });
+        });
 
-            services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlite("Data Source=TranslationAppDatabase.db"));
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite("Data Source=TranslationAppDatabase.db"));
 
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddScoped<ITranslatorService, TranslatorService>();
-            services.AddScoped<ITranslationJobService, TranslationJobService>();
-        }
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<ITranslatorService, TranslatorService>();
+        services.AddScoped<ITranslationJobService, TranslationJobService>();
+    }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TranslationManagement.Api v1"));
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TranslationManagement.Api v1"));
 
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
     }
 }
